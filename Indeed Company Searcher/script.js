44fc2f99222160716a16b30c09d7e77c59c58a9d
Kody <65751361+Kody-12@users.noreply.github.com>
2023-11-16 14:02:22 -0700

    Add files via upload

---
+++ Indeed Company Searcher/script.js
@@ -0,0 +1,13 @@
+// creates right click menu based off highlighted word 
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'thing',
+    title: "Search \"%s\" on Indeed!",
+    contexts: ["selection"],
+  });
+});
+
+// takes the highlighted word and opens indeed tab
+chrome.contextMenus.onClicked.addListener(function (word) {
+  chrome.tabs.create({ url: "https://www.indeed.com/companies/search?q=" + word.selectionText });
+});
