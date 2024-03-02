25d0ca56fea04fd1a12460b82767859ced4ae931
Qhotti <kody.pudney@gmail.com>
2024-03-02 12:03:11 -0700

    script update

--- script.js
+++ script.js
@@ -11,17 +11,3 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener(function (word) {
   chrome.tabs.create({ url: "https://www.indeed.com/companies/search?q=" + word.selectionText });
 });
-
-// // creates right click menu based off highlighted word 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.contextMenus.create({
-//     id: 'glassdoor',
-//     title: "Search \"%s\" on Glassdoor!",
-//     contexts: ["selection"],
-//   });
-// });
-
-// // takes the highlighted word and opens indeed tab
-// chrome.contextMenus.onClicked.addListener(function (word) {
-//   chrome.tabs.create({ url: "https://www.glassdoor.com/Search/results.htm?keyword=" + word.selectionText });
-// });
