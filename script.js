eb2a84175b7a05eb2057412b925904230904e059
Qhotti <kody.pudney@gmail.com>
2024-03-01 17:38:08 -0700

    Name Change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // creates right click menu based off highlighted word 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: 'thing',
+    id: 'indeed',
     title: "Search \"%s\" on Indeed!",
     contexts: ["selection"],
   });
@@ -11,3 +11,17 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener(function (word) {
   chrome.tabs.create({ url: "https://www.indeed.com/companies/search?q=" + word.selectionText });
 });
+
+// // creates right click menu based off highlighted word 
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.contextMenus.create({
+//     id: 'glassdoor',
+//     title: "Search \"%s\" on Glassdoor!",
+//     contexts: ["selection"],
+//   });
+// });
+
+// // takes the highlighted word and opens indeed tab
+// chrome.contextMenus.onClicked.addListener(function (word) {
+//   chrome.tabs.create({ url: "https://www.glassdoor.com/Search/results.htm?keyword=" + word.selectionText });
+// });
