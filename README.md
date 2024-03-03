b185ecb30c4132d663c3ced3a3801d26cdb2258c
Qhotti <kody.pudney@gmail.com>
2024-03-02 21:32:33 -0700

    v2 changes

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 Check out my official extension on the chrome webstore!
 https://chromewebstore.google.com/detail/company-crawler/bgmhndibpboacneibaelceohhilelpab?hl=en&authuser=1
 
-A fun little extension I made that helps me look up a company's reviews easily on Indeed.
+A fun little extension I made that helps me look up a company's reviews easily on Indeed, Linkedin, and Glassdoor.
 
 COMING SOON
-- more companies (Glassdoor and LinkedIn)
 - firefox support
 - gifs showing how to use
--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-// creates right click menu based off highlighted word 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'indeed',
-    title: "Search \"%s\" on Indeed!",
-    contexts: ["selection"],
-  });
+
+chrome.runtime.onInstalled.addListener(function () {
+
+
+  // list of each provider
+  let names = [
+    'Indeed',
+    'LinkedIn',
+    'Glassdoor',
+    'All'
+  ];
+
+
+  // creates a context menu for each of the providers
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    let title = "Search \"%s\" on " + name +"!";
+
+    chrome.contextMenus.create({
+      title: title,
+      contexts: ['selection'],
+      id: name,
+    });
+  }
 });
 
-// takes the highlighted word and opens indeed tab
-chrome.contextMenus.onClicked.addListener(function (word) {
-  chrome.tabs.create({ url: "https://www.indeed.com/companies/search?q=" + word.selectionText });
-});
+
+// takes the highlighted word and opens tab
+ chrome.contextMenus.onClicked.addListener(function (word) {
+
+  // object that gives names to the links
+  let links = {
+    Indeed: 'https://www.indeed.com/companies/search?q=',
+    LinkedIn:'https://www.linkedin.com/search/results/companies/?keywords=',
+    Glassdoor: 'https://www.glassdoor.com/Search/results.htm?keyword='
+  };
+
+const site = String(word.menuItemId); // this finds the id from the site selected and makes it look purty
+
+// checks if the site is in the object
+  if (site in links === true){
+   chrome.tabs.create({ url: links[site] + word.selectionText });
+  } else if (site == 'All'){
+    for (x in links){ // this opens all the links if you click all
+      chrome.tabs.create({ url: links[x] + word.selectionText });;
+    };
+    }
+    
+ });
